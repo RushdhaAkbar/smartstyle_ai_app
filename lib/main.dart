67eed630fb8a37a8c635c08a4fd382d01c4224ad
RushdhaAkbar <rushdhaakbar82@gmail.com>
2025-09-03 22:32:58 +0530

    fixed gradle issues

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
+import 'providers/product_provider.dart';
 import 'screens/login_screen.dart';
 
 void main() {
@@ -13,12 +14,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => AuthProvider(), 
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+      ],
       child: MaterialApp(
         title: 'SmartStyle AI',
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: LoginScreen(), 
+        home: LoginScreen(),
       ),
     );
   }
